03ad7af06d862f43119cbeacc85ec3999ec249ff
Hammad-87 <pakswim@gmail.com>
2020-08-20 01:01:11 +0200

    updated

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 import Home from './pages/Home'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import routes from './routes';
+import Router from './routes/Router';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        {routes.map((route, index)=> {
-          return(
-            <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            render={(props)=>{
-              return(
-                <route.layout {...props}>
-                  <route.component {...props} />
-                </route.layout>
-              )
-            }}
-            />
-          )
-        })}
-        <Redirect to={"/"} />
-      </Switch>
-    </Router>
+    <>
+    <Router />
+    {/* <Home /> */}
+    </>
   );
 }
 
